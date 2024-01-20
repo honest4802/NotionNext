efd2266b391b2353cfac1bf20dd8fbee14aec1af
sycicu <88899141+honest4802@users.noreply.github.com>
2024-01-20 22:49:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '心情如歌', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '没有什么好说的🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.syong.link', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '心情如歌,博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '心情如歌,博客，数码', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
